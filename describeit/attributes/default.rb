a2844bdcd8d4b47a525c90820af032aa1ccf62dc
Andre Cantero <thecantero@gmail.com>
2016-11-09 05:53:46 +0800

    nginx and default path

--- describeit/attributes/default.rb
+++ describeit/attributes/default.rb
@@ -7,9 +7,7 @@ default['nodejs']['version'] = '4.5.0'
 
 default['redis']['version'] = '3.2.3'
 
-default['app'] = 'describeit-sample-app'
-
-default['stage'] = 'development'
+default['app'] = 'describeit'
 
 default['database'] = {}
 
